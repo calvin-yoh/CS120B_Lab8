2f77c8b6ed2af1a5446939c8deb084d9208dab96
Calvin Yoh <flashfire340@gmail.com>
2020-05-16 22:06:00 -0700

    Update main.c

--- source/main.c
+++ source/main.c
@@ -23,7 +23,7 @@ unsigned short threshold = 0x00;
 
 int main(void)
 {
-	threshold = (~max / 8);
+	threshold = (max / 8);
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
 	DDRD = 0xFF; PORTD = 0x00;
